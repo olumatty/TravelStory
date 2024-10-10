32e80f22d44d3b9196dab0da94511a2a3badfd18
Matthew Adebayo <olumatty@gmail.com>
2024-10-10 02:31:43 +0100

    second commit

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,11 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
 const User = require("./models/user.model");
+const TravelStory = require("./models/travelStory.model");
+const { authenticateToken } = require("./utilities");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
@@ -21,10 +24,12 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.get("/create-account", async (req, res) => {
-  const { fullname, email, password } = req.body;
+//Create an account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
 
-  if (!fullname || !email || !password) {
+  //check if account already exist`
+  if (!fullName || !email || !password) {
     return res
       .status(400)
       .json({ error: true, message: "All field are required" });
@@ -35,8 +40,119 @@ app.get("/create-account", async (req, res) => {
       .status(400)
       .json({ error: true, message: "User already exists" });
   }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    fullName,
+    email,
+    password: hashedPassword,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign(
+    { userId: user._id },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "72h",
+    }
+  );
+
+  return res.status(201).json({
+    error: false,
+    user: { fullName: user.fullName, email: user.email },
+    accessToken,
+    message: "Registration Successful",
+  });
 });
 
+//login to an account
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email is required" });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({ message: "Invalid Credential" });
+  }
+
+  const accessToken = jwt.sign(
+    { userId: user._id },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "72h",
+    }
+  );
+
+  return res.json({
+    error: false,
+    message: "Login Successful",
+    user: { fullName: user.fullName, email: user.email },
+    accessToken,
+  });
+});
+
+//Get-user
+app.get("/get-user", authenticateToken, async (req, res) => {
+  const { userId } = req.user;
+
+  const isUser = await User.findOne({ _id: userId });
+
+  if (!isUser) {
+    return res.sendStatus(401);
+  }
+
+  return res.json({
+    user: isUser,
+    message: "",
+  });
+});
+
+//Add Travel Story
+app.post("/add-travel-story", authenticateToken, async (req, res) => {
+  const { title, story, visitedLocation, imageUrl, visitedDate } = req.body;
+  const { userId } = req.user;
+
+  //Validate required fields
+  if (!title || !story || !visitedLocation || !imageUrl || !visitedDate) {
+    return res.status(400).json({
+      error: true,
+      message: "All fields are required",
+    });
+  }
+  //Convert visitedDate from milliescondsto Date Object
+  const parsedVisitedDate = new Date(parseInt(vistedDate));
+
+  try {
+    const travelStory = new TravelStory({
+      title,
+      story,
+      visitedLocation,
+      imageUrl,
+      userId,
+      visitedDate: parsedVisitedDate,
+    });
+    await travelStory.save();
+    return res
+      .status(201)
+      .json({ story: travelStory, message: "Added Successfully" });
+  } catch (error) {
+    return res.status(400).json({
+      error: true,
+      message: error.message,
+    });
+  }
+});
 app.listen(3000);
 
 module.exports = app;
